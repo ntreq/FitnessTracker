d73844d63427441209975f7bd24f3aa5f268223f
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-04-18 11:46:50 +0200

    Modify Event.java

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,47 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "events")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "startTime", nullable = false)
+    private LocalDate startTime;
+    @Column(name = "endTime", nullable = false)
+    private LocalDate endTime;
+    @Column(name = "country", nullable = false)
+    private String country;
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    public Event(@Nullable Long id, String name, String description, LocalDate startTime, LocalDate endTime, String country, String city) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
