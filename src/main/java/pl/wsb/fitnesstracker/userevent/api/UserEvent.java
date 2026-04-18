00962fd5a978ad480d670bc6f35b5115dadf22ab
psmoliga <wrx106860@student.wroclaw.merito.pl>
2026-04-18 11:49:14 +0200

    Dodanie encji UserEvent i Workoutsession do modelu

---
+++ src/main/java/pl/wsb/fitnesstracker/userevent/api/UserEvent.java
@@ -0,0 +1,25 @@
+package pl.wsb.fitnesstracker.userevent.api;
+
+import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.event.Event;
+import pl.wsb.fitnesstracker.training.api.Training;
+
+@Entity
+@Table(name = "user_events")
+public class UserEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training training;
+
+
+
+}
--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,58 @@
-package pl.wsb.fitnesstracker.workoutsession;
+    package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.training.api.Training;
+import pl.wsb.fitnesstracker.user.api.User;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import java.time.LocalDateTime;
+
+
+    @Entity
+@Table(name = "workout_sessions")
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training training;
+
+    @Column(name="timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "startLatitude", nullable = false)
     private double startLatitude;
+
+    @Column(name = "startLongitude", nullable = false)
     private double startLongitude;
+
+    @Column(name = "endLatitude", nullable = true)
     private double endLatitude;
+
+    @Column(name = "endLongitude", nullable = true)
     private double endLongitude;
+
+    @Column(name = "altitude", nullable = false)
     private double altitude;
+
+    public WorkoutSession(
+                Training training,
+                LocalDateTime timestamp,
+                double startLatitude,
+                double startLongitude,
+                double endLatitude,
+                double endLongitude,
+                double altitude) {
+        this.training = training;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
+
 }
